47bccb48720551d59f12d887aedd67f67f84b64a
mixmaster7 <mjone352@gamil.com>
2021-04-19 12:36:31 -0500

    contact us form

---
+++ src/components/footer/contactus.tsx
@@ -0,0 +1,38 @@
+import { Row, Col, Input, Space, Button } from "antd";
+
+export const Contactus: React.FC = () => {
+  const { TextArea } = Input;
+  return (
+    <div className="contact-us">
+      <p className="title">Contact Us</p>
+
+      <p className="sub-title">Contact us with any question or query</p>
+
+      <div className="contact-form">
+        <Row justify="space-around">
+          <Col xs={6}>
+            <Space direction="vertical" size="large">
+              <Input placeholder="Your Name" />
+              <Input placeholder="Your Email" />
+            </Space>
+          </Col>
+
+          <Col xs={16}>
+            <TextArea rows={4} placeholder="Your Message" />
+          </Col>
+
+          <Col xs={18}>
+            <p>
+              By pressing the submit button you are accepting our{" "}
+              <a href="#">Privacy Policy</a> and our <a href="#">Term of use</a>
+            </p>
+          </Col>
+
+          <Col xs={4} className="submit">
+            <Button className="btn-large">SEND A MESSAGE</Button>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Space } from "antd";
+import { Contactus } from "./contactus";
 
 export const Footer: React.FC = () => {
   return (
     <footer>
-      <Space align="center" size={100}>
-        <img src="imgs/logo.png" alt="" />
+      <Contactus></Contactus>
+      <div className="footer-bar">
+        <Space align="center" size={100}>
+          <img src="imgs/logo.png" alt="" />
 
-        <p>
-          This website is an independent comparison site that aims to help
-          consumers find the matching product for their needs. We are able to
-          maintain a free service by charging an advertising fee to featured
-          brands on our site. These advertising fees might impact the placement
-          of the brands and scoring. We make effort to present up-to-date
-          information; however, we do not compare or include all service
-          providers in the market. All rights reserved @ 2019
-        </p>
-      </Space>
+          <p>
+            This website is an independent comparison site that aims to help
+            consumers find the matching product for their needs. We are able to
+            maintain a free service by charging an advertising fee to featured
+            brands on our site. These advertising fees might impact the
+            placement of the brands and scoring. We make effort to present
+            up-to-date information; however, we do not compare or include all
+            service providers in the market. All rights reserved @ 2019
+          </p>
+        </Space>
+      </div>
     </footer>
   );
 };
