b96d4e70772dcd453d45265b7b4b14db24ffd583
mixmaster7 <mjone352@gmail.com>
2021-04-20 18:51:34 -0500

    mobile design bug fix

---
+++ pages/review.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Footer, Header } from '@components';
+
+const Review: React.FC = () => {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Review;
--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Link from 'next/link';
 
 export const Logo: React.FC = () => {
-  return <img src="imgs/logo.png" alt="logo" />;
+  return (
+    <Link href="/">
+      <img src="imgs/logo.png" alt="logo" style={{ cursor: 'pointer' }} />
+    </Link>
+  );
 };
