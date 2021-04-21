60b033a0c055ca4aa28a32c2e92fbb992792f6ba
mixmaster7 <mjone352@gmail.com>
2021-04-21 13:54:31 -0500

    url fix

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export const Logo: React.FC = () => {
   return (
-    <Link href="/">
+    <Link href="//">
       <img src="imgs/logo.png" alt="logo" style={{ cursor: 'pointer' }} />
     </Link>
   );
