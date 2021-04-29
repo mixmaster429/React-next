919aa8339d545198e117ffe348dd04305c38f71b
irfan7junior <mdirfan548@gmail.com>
2021-04-29 08:53:18 +0530

    adjusted entire repo with .prettierrs config

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card as AntdCard } from 'antd';
 import { CardProps } from 'antd/lib/card';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const Card: React.FC<CardProps> = ({ title, children, ...rest }) => {
   return (
     <AntdCard title={title} bordered={false}>
