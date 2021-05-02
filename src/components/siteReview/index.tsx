32ef490e72da20de7291dba3e1c99fcf474ed50d
MD Irfan <mdirfan548@gmail.com>
2021-05-02 22:48:03 +0530

    completed adding-links to review card (#10)

--- src/components/siteReview/index.tsx
+++ src/components/siteReview/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Rate, Button, Row, Col } from 'antd';
 import { GlobalOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+import { BiRightArrowAlt } from 'react-icons/bi';
 
 interface ReviewsProps {
   number: string;
@@ -15,12 +17,17 @@ export const SiteReview: React.FC<ReviewsProps> = ({ number, brandImage }) => {
           <img src={number} alt="number" className="number" />
         </Col>
         <Col span={12} lg={11}>
-          <img src={brandImage} alt="brand" className="brand"/>
+          <img src={brandImage} alt="brand" className="brand" />
         </Col>
         <Col span={9} lg={10}>
           <div className="rating">
             <p className="rating-value">Rating 9.9</p>
             <Rate disabled defaultValue={5} />
+            <Link href="/review">
+              <a href="">
+                Read Review <BiRightArrowAlt />
+              </a>
+            </Link>
           </div>
         </Col>
       </Row>
